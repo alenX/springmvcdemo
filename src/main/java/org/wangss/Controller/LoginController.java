345244191f05414253a9a3013ca4a66a9e09a29c
wangss <genhaoai@gmail.com>
2017-07-16 20:53:39 +0800

    Shiro权限修改

--- src/main/java/org/wangss/Controller/LoginController.java
+++ src/main/java/org/wangss/Controller/LoginController.java
@@ -1,7 +1,16 @@
 package org.wangss.Controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.wangss.Mapper.SysUserMapper;
+import org.wangss.Model.SysUser;
+import org.wangss.Model.SysUserExample;
+
 
 /**登录Controller
  * Created by wangss on 17-7-16.
@@ -9,4 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("login")
 @Controller
 public class LoginController {
+
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
+
+    @RequestMapping("login")
+    public String login(@RequestParam String username,@RequestParam String password){
+
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        Subject subject = SecurityUtils.getSubject();
+        subject.login(token);
+        SysUserExample example = new SysUserExample();
+        example.createCriteria().andUsernameEqualTo(username);
+        SysUser user = sysUserMapper.selectByExample(example).get(0);
+        subject.getSession().setAttribute("user",user);
+        return  "succ";
+    }
 }
